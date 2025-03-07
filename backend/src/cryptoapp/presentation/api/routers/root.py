c28feee3b1443c9876d66d6842dce21d319c75b0
Krak3nDev <bihichnazar@gmail.com>
2025-03-08 00:27:04 +0200

    chore(metrics): add metrics

--- backend/src/cryptoapp/presentation/api/routers/root.py
+++ backend/src/cryptoapp/presentation/api/routers/root.py
@@ -1,3 +1,5 @@
+import logging
+
 from fastapi import APIRouter
 
 from cryptoapp.presentation.api.routers.auth import auth_router
@@ -10,6 +12,7 @@ root_router.include_router(user_router)
 root_router.include_router(auth_router)
 
 
-@root_router.get("/")
+@root_router.get("/health")
 async def root() -> dict[str, str]:
+    logging.error("Hello from backend!")
     return {"message": "success"}
