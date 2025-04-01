5f4d3dbd5958772fb0e15cdc717a58e71e3ea5f5
Pasha-Makarenko <pashamakarenko2007@gmail.com>
2025-04-01 14:05:24 +0300

    Add auth errors and validators

--- frontend/src/app/consts/endpoints.ts
+++ frontend/src/app/consts/endpoints.ts
@@ -1,4 +1,4 @@
-export const BASE_API_URL = "http://localhost:8080"
+export const BASE_API_URL = "http://localhost:8000"
 
 export const ENDPOINTS = {
   auth: {
--- frontend/src/app/pages/auth/auth.component.ts
+++ frontend/src/app/pages/auth/auth.component.ts
@@ -1,11 +1,6 @@
 import { Component, inject, signal } from "@angular/core"
 import { Router, RouterLink } from "@angular/router"
-import {
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators
-} from "@angular/forms"
+import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms"
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome"
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons"
 import { AuthService } from "../../api/auth/auth.service"
@@ -14,10 +9,18 @@ import {
   LoginBodyRequest,
   RegisterBodyRequest
 } from "../../api/auth/auth.interface"
+import { authFormValidators } from "./form.validators"
+import { AuthErrorComponent } from "./error/auth-error.component"
+import { HttpErrorResponse } from "@angular/common/http"
 
 @Component({
   selector: "app-auth",
-  imports: [ReactiveFormsModule, RouterLink, FontAwesomeModule],
+  imports: [
+    ReactiveFormsModule,
+    RouterLink,
+    FontAwesomeModule,
+    AuthErrorComponent
+  ],
   templateUrl: "./auth.component.html",
   styleUrl: "./auth.component.scss"
 })
@@ -25,13 +28,21 @@ export class AuthComponent {
   router = inject(Router)
   authService = inject(AuthService)
 
-  isRegister = false
-  authForm = new FormGroup({
-    username: new FormControl<string>("", [Validators.required]),
-    email: new FormControl<string>("", [Validators.required, Validators.email]),
-    password: new FormControl<string>("", [Validators.required]),
-    confirmPassword: new FormControl<string>("", [Validators.required])
-  })
+  isRegister = signal(false)
+
+  validators = authFormValidators(this.isRegister)
+  authForm = new FormGroup(
+    {
+      username: new FormControl<string>("", this.validators.username),
+      email: new FormControl<string>("", this.validators.email),
+      password: new FormControl<string>("", this.validators.password),
+      confirmPassword: new FormControl<string>(
+        "",
+        this.validators.confirmPassword
+      )
+    },
+    this.validators.passwordMatch
+  )
   isPasswordVisible = signal({
     password: false,
     confirmPassword: false
@@ -40,45 +51,63 @@ export class AuthComponent {
     eye: faEye,
     eyeSlash: faEyeSlash
   }
+  submitError = signal<object | null>(null)
 
   ngOnInit() {
     if (this.router.url.includes(ROUTES.register)) {
-      this.isRegister = true
+      this.isRegister.set(true)
     }
   }
 
   onSubmit() {
-    if (this.authForm.invalid || !this.passwordMatch()) {
+    if (this.authForm.invalid) {
       return
     }
 
-    if (this.isRegister) {
+    if (this.isRegister()) {
       const payload: RegisterBodyRequest = {
         username: this.authForm.value.username!,
         password: this.authForm.value.password!,
         email: this.authForm.value.email!
       }
 
-      this.authService.register(payload).subscribe(() => {
-        this.router.navigate([ROUTES.confirm])
-      })
+      this.authService
+        .register(payload)
+        .subscribe(
+          this.submitResponseHandlers.success,
+          this.submitResponseHandlers.error
+        )
     } else {
       const payload: LoginBodyRequest = {
         username: this.authForm.value.username!,
         password: this.authForm.value.password!
       }
 
-      this.authService.login(payload).subscribe(() => {
-        this.router.navigate([ROUTES.home])
-      })
+      this.authService
+        .login(payload)
+        .subscribe(
+          this.submitResponseHandlers.success,
+          this.submitResponseHandlers.error
+        )
     }
   }
 
-  passwordMatch(): boolean {
-    return (
-      this.authForm.get("password")?.value ===
-      this.authForm.get("confirmPassword")?.value
-    )
+  submitResponseHandlers = {
+    success: (_res: object) => {
+      this.router.navigate(this.isRegister() ? [ROUTES.confirm] : [ROUTES.home])
+    },
+    error: (error: HttpErrorResponse) => {
+      if (error.status === 404) {
+        this.submitError.set({
+          submitInvalid: true
+        })
+        return
+      }
+
+      this.submitError.set({
+        common: true
+      })
+    }
   }
 
   showPassword(field: "password" | "confirmPassword") {
