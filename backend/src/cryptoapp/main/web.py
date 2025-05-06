935b54459ad10a29a8d3dd41e73a4e47a1a33249
Pasha-Makarenko <pashamakarenko2007@gmail.com>
2025-05-06 21:44:33 +0300

    fix port

--- backend/src/cryptoapp/main/web.py
+++ backend/src/cryptoapp/main/web.py
@@ -39,8 +39,8 @@ def create_app(broker: AsyncBroker, container: AsyncContainer) -> FastAPI:
     origins_dev = [
         "http://localhost",
         "http://127.0.0.1",
-        "http://localhost:4200",
-        "http://127.0.0.1:4200",
+        "http://localhost:4242",
+        "http://127.0.0.1:4242",
     ]
 
     app.add_middleware(
--- frontend/src/app/consts/endpoints.ts
+++ frontend/src/app/consts/endpoints.ts
@@ -1,4 +1,4 @@
-export const BASE_API_URL = "http://localhost/api"
+export const BASE_API_URL = "http://localhost:8000"
 
 export const ENDPOINTS = {
   auth: {
